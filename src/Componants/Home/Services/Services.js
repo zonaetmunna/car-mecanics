f32c58f6ab17d4d2d4964928d5fbb87d8ad11fbe
zonaetmunna <zonaetmonna@gmail.com>
2021-10-13 02:13:30 +0600

    first commit

---
+++ src/Componants/Home/Services/Services.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css'
+
+
+
+const Services = () => {
+     const [services, setServices] = useState([])
+     useEffect(() => {
+          fetch('services.json')
+               .then(res => res.json())
+               .then(data => setServices(data))
+     }, [])
+
+
+
+
+     return (
+          <div >
+               <div className="services-container">
+                    {
+                         services.map(service => <Service
+
+                              key={service.id}
+                              service={service}
+                         ></Service>)
+                    }
+
+               </div>
+          </div>
+     );
+};
+
+export default Services;
